6ac2459c991d15b886d7a8f831880b481c2a1a16
skumar <skumar@boompop.com>
2023-04-15 18:22:21 +0530

    sdd

--- app/Models/ModelMake.php
+++ app/Models/ModelMake.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class ModelMake extends Model
 {
     use HasFactory;
-    protected $fillable = ['name','make_id'];
+    protected $fillable = ['name', 'make_id'];
     protected $casts = [
         'created_at' => 'datetime:M d, Y h:i:s',
         'updated_at' => 'datetime:M d, Y h:i:s',
@@ -19,6 +19,13 @@ class ModelMake extends Model
     }
     public function makeModels()
     {
-        return $this->hasMany(MakeModel::class,'make_id','id');
+        return $this->hasMany(MakeModel::class, 'make_id', 'id');
+    }
+    public function getMakes($make, $type)
+    {
+        $data = MakeModel::where(['make_id' => $make, 'type' => $type])->first();
+        if ($data) {
+            return $data->name;
+        }
     }
 }
