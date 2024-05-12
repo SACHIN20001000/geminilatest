5553a74f5042686eb8955e94490f305074717c4e
Sachin kumar <sachindts98@gmail.com>
2024-05-12 19:22:09 +0530

    remove post

--- resources/views/admin/policy/new-index.blade.php
+++ resources/views/admin/policy/new-index.blade.php
@@ -696,7 +696,6 @@
             ajax: {
                 url: "{{ route('new-policy.index') }}",
                 cache: false,
-                method:'POST',
                 data: function(d) {
                     d.duplicate = duplicate;
                     d.id = id;
